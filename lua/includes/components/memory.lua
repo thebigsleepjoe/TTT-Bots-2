6d344d73e52b5172da283d09ca9ac8e73e3d4dfc
thebigsleepjoe <warmharddeath1@gmail.com>
2023-04-26 02:35:02 -0700

    Memory init

---
+++ lua/includes/components/memory.lua
@@ -0,0 +1,47 @@
+--[[
+This module is not intended to store everything bot-related, but instead store bot-specific stuff that
+is refreshed every round. Things like where the bot last saw each player, etc.
+]]
+TTTBots = TTTBots or {}
+TTTBots.Components.BotMemory = TTTBots.BotMemory or {}
+
+local lib = TTTBots.Lib
+local BotMemory = TTTBots.Components.BotMemory
+
+
+function BotMemory:New(bot)
+    local newMemory = {}
+    setmetatable(newMemory, {
+        __index = function(t, k) return BotMemory[k] end,
+    })
+    newMemory:Initialize(bot)
+
+    local dbg = lib.GetConVarBool("debug_misc")
+    if dbg then
+        print("Initialized Memory for bot " .. bot:Nick())
+    end
+
+    return newMemory
+end
+
+function BotMemory:ResetMemory()
+    self.playerPositions = {}
+end
+
+function BotMemory:Initialize(bot)
+    print("Initializing")
+    bot.components = bot.components or {}
+    bot.components.memory = self
+
+    self:ResetMemory()
+
+    self.bot = bot
+end
+
+function BotMemory:UpdatePlayerPositions()
+
+end
+
+function BotMemory:Think()
+    self:UpdatePlayerPositions()
+end
