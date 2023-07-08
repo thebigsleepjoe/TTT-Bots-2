d3c9132b5d7b17ac66a8bd4e4002de909f7163c7
thebigsleepjoe <warmharddeath1@gmail.com>
2023-07-07 19:06:22 -0700

    Boilerplate

---
+++ lua/includes/behaviors/investigatenoise.lua
@@ -0,0 +1,19 @@
+---@class InvestigateNoise
+TTTBots.Behaviors.InvestigateNoise = {}
+
+local lib = TTTBots.Lib
+
+---@class InvestigateNoise
+local InvestigateNoise = TTTBots.Behaviors.InvestigateNoise
+
+function InvestigateNoise:OnStart(bot) end
+
+function InvestigateNoise:OnRunning(bot) end
+
+function InvestigateNoise:Validate(bot) end
+
+function InvestigateNoise:OnFailure(bot) end
+
+function InvestigateNoise:OnSuccess(bot) end
+
+function InvestigateNoise:OnEnd(bot) end
