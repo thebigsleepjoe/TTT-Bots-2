d4bccf9b2362a3bd936b5e3a92caf7b5397daa15
thebigsleepjoe <warmharddeath1@gmail.com>
2023-07-08 00:25:51 -0700

    minor cleanup of unused var

--- lua/includes/behaviors/investigatenoise.lua
+++ lua/includes/behaviors/investigatenoise.lua
@@ -6,6 +6,19 @@ local lib = TTTBots.Lib
 ---@class InvestigateNoise
 local InvestigateNoise = TTTBots.Behaviors.InvestigateNoise
 
+InvestigateNoise.INVESTIGATE_CATEGORIES = {
+    "Gunshot",
+    "Death",
+    "C4Beep",
+    "Explosion"
+}
+
+function InvestigateNoise:GetRecentSounds(bot)
+    ---@type CMemory
+    local memory = bot.components.memory
+    local sounds = memory:GetRecentSounds()
+end
+
 function InvestigateNoise:OnStart(bot) end
 
 function InvestigateNoise:OnRunning(bot) end
