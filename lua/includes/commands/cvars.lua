ec969655b82182bd2d8131635d9cfdfe342c9a5f
thebigsleepjoe <warmharddeath1@gmail.com>
2023-11-05 19:39:23 -0800

    Update bot reaction speed cvar

--- lua/includes/commands/cvars.lua
+++ lua/includes/commands/cvars.lua
@@ -33,7 +33,7 @@ bot_cvar("difficulty", "3",
     "A difficulty integer between 1-5; higher = harder. This affects trait selection and aim speed, reaction speed, and KOS callout chances.")
 bot_cvar("kos_limit", "2", "The upper bound of KOS calls an individual, bot or player, can make per round. Before the bots ignore them, at least. Used to deter trolls.")
 bot_cvar("redhanded_time", "3", "This is the number of seconds that a player is silently marked KOS by bots after killing a non-evil class. This is technically a cheat, but makes the bots more engaging.")
-bot_cvar("reaction_speed", "0.4", "The base time, in seconds, a bot will take before attacking a newly assigned target. Higher means easier gameplay. THIS INVERSELY SCALES WITH DIFFICULTY AUTOMATICALLY.")
+bot_cvar("reaction_speed", "0.5", "The base time, in seconds, a bot will take before attacking a newly assigned target. Higher means easier gameplay. THIS INVERSELY SCALES WITH DIFFICULTY AUTOMATICALLY.")
 bot_cvar("reaction_speed_traitors_lessened", "1", "If set to 1, traitor bots will have a superior reaction speed. This is technically a cheat, but makes the bots more engaging.")
 
 -- Noise cvars
@@ -97,7 +97,7 @@ bot_cvar("allow_leaving", "1",
 -- Pathfinding cvars
 bot_cvar("pathfinding_cpf", "50",
     "Don't change this unless you know what you are doing. How many pathfinding calculations to do per frame. Higher values = more CPU usage, but faster pathfinding.")
-bot_cvar("pathfinding_cpf_scaling", "1",
+bot_cvar("pathfinding_cpf_scaling", "0",
     "Don't change this unless you know what you are doing. Should we dynamically multiply the pathfinding calculations per frame by the number of bots? (e.g. 50 cpf * 2 bots = 100 cpf)")
 bot_cvar("enable_rdm", "0", -- TODO: Implement RDM
     "Enables RDM (random deathmatch). This isn't advised for most situations, but can offer some extra variety should you want it.")
