392afbe66cafeb042e8482e906cf6ea7291ffff2
thebigsleepjoe <warmharddeath1@gmail.com>
2023-12-14 01:19:54 -0800

    more dialog templates

--- lua/tttbots2/data/dialogtemplates.lua
+++ lua/tttbots2/data/dialogtemplates.lua
@@ -1,12 +1,25 @@
 local Dialog = TTTBots.Locale.Dialog ---@type TTTBots.DialogModule
 
--- Greet each other 1
+-- Greet each other v1
 Dialog.NewTemplate(
     "Greetings1",
     2,
     {
         Dialog.NewLine("GreetNext", 1),
         Dialog.NewLine("GreetLast", 2),
+    },
+    false
+)
+
+-- Greet each other v2
+Dialog.NewTemplate(
+    "Greetings1",
+    2,
+    {
+        Dialog.NewLine("GreetNext", 1),
+        Dialog.NewLine("GreetLast", 2),
+        Dialog.NewLine("WhatsUp", 1),
+        Dialog.NewLine("WhatsUpResponse", 2),
     },
     false
 )
