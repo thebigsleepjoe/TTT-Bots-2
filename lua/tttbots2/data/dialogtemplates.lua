b16ae8d6231d71c752fbc3e2077bec6a9c048a70
thebigsleepjoe <warmharddeath1@gmail.com>
2023-12-14 01:30:35 -0800

    Update dialog templates and chat locale

--- lua/tttbots2/data/dialogtemplates.lua
+++ lua/tttbots2/data/dialogtemplates.lua
@@ -11,7 +11,7 @@ Dialog.NewTemplate(
     false
 )
 
--- Greet each other v2
+-- Greet each other v2 - what's up
 Dialog.NewTemplate(
     "Greetings1",
     2,
@@ -20,6 +20,30 @@ Dialog.NewTemplate(
         Dialog.NewLine("GreetLast", 2),
         Dialog.NewLine("WhatsUp", 1),
         Dialog.NewLine("WhatsUpResponse", 2),
+    },
+    false
+)
+
+
+-- Greet each other v3 - how are you
+Dialog.NewTemplate(
+    "Greetings1",
+    2,
+    {
+        Dialog.NewLine("GreetNext", 1),
+        Dialog.NewLine("GreetLast", 2),
+        Dialog.NewLine("HowAreYou", 1),
+        Dialog.NewLine("HowAreYouResponse", 2),
+    },
+    false
+)
+
+-- Greet each other v4 - unreciprocated
+Dialog.NewTemplate(
+    "Greetings1",
+    2,
+    {
+        Dialog.NewLine("GreetNext", 1),
     },
     false
 )
