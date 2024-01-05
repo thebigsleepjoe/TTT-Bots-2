73a88e4cbdc38cd3a87d31eb23c46c735e01ac67
thebigsleepjoe <warmharddeath1@gmail.com>
2024-01-05 01:26:48 -0800

    Add IsBreakable function to BotObstacleTracker for #33 (fix)

--- lua/tttbots2/components/sv_obstacletracker.lua
+++ lua/tttbots2/components/sv_obstacletracker.lua
@@ -131,6 +131,19 @@ function BotObstacleTracker:GetBlocking()
     return self.blocking
 end
 
+---Get whether or not an entity is breakable. Warning: does not null-check.
+---@param entity Entity
+---@return boolean
+function BotObstacleTracker.IsBreakable(entity)
+    local vals = entity:GetKeyValues()
+    local health_min = 1
+    local health_max = 300 -- Don't bother breaking something that has more than 300 health
+    return (vals.health and vals.health > health_min and vals.health < health_max) or false
+    -- table.insert(db_break, entity)
+    -- else
+    -- table.insert(db_unbreak, entity)
+end
+
 ----------------------------------------
 -- STATIC METHODS / FIELDS
 ----------------------------------------
@@ -142,7 +155,6 @@ timer.Create("TTTBots.Components.ObstacleTracker_Breakables", 1.5, 0, function()
     local db_break = {}
     local db_unbreak = {}
 
-    local dbg = lib.GetConVarBool("debug_obstacles")
 
     -- for i, v in pairs(ents.GetAll()) do
     --     print(v:GetClass())
@@ -162,14 +174,11 @@ timer.Create("TTTBots.Components.ObstacleTracker_Breakables", 1.5, 0, function()
     table.Add(phys, ents.FindByClass("func_physbox"))
 
     for _, entity in pairs(phys) do
-        local vals = entity:GetKeyValues()
-        if vals.health and vals.health > 1 then
-            table.insert(db_break, entity)
-        else
-            table.insert(db_unbreak, entity)
-        end
+        local isBreakable = BotObstacleTracker.IsBreakable(entity)
+        table.insert(isBreakable and db_break or db_unbreak, entity)
     end
 
+    local dbg = lib.GetConVarBool("debug_obstacles")
     if dbg then
         print(#db_break .. " breakables, " .. #db_unbreak .. " unbreakables.")
     end
