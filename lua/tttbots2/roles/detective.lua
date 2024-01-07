59a5f724739b4e1f38088242c6933577f517558c
thebigsleepjoe <warmharddeath1@gmail.com>
2024-01-07 15:37:21 -0800

    Fixed bug where innocents would not attach each other

--- lua/tttbots2/roles/detective.lua
+++ lua/tttbots2/roles/detective.lua
@@ -3,6 +3,8 @@ detective:SetDefusesC4(true)
 detective:SetCanHaveRadar(true)
 detective:SetTeam(TEAM_INNOCENT)
 detective:SetBTree(TTTBots.Behaviors.DefaultTrees.detective)
+detective:SetUsesSuspicion(true)
+detective:SetAppearsPolice(true)
 TTTBots.Roles.RegisterRole(detective)
 
 return true
--- lua/tttbots2/roles/innocent.lua
+++ lua/tttbots2/roles/innocent.lua
@@ -2,6 +2,9 @@ local innocent = TTTBots.RoleData.New("innocent")
 innocent:SetDefusesC4(true)
 innocent:SetTeam(TEAM_INNOCENT)
 innocent:SetBTree(TTTBots.Behaviors.DefaultTrees.innocent)
+innocent:SetUsesSuspicion(true)
+innocent:SetAlliedRoles({})
+innocent:SetAlliedTeams({})
 TTTBots.Roles.RegisterRole(innocent)
 
 return true
