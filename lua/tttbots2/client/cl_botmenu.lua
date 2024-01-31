a2b3155f5810a4576b43aa1ea75696829c636b1a
thebigsleepjoe <warmharddeath1@gmail.com>
2024-01-31 01:16:43 -0800

    Add bot names GUI

--- lua/tttbots2/client/cl_botmenu.lua
+++ lua/tttbots2/client/cl_botmenu.lua
@@ -22,20 +22,103 @@ local function onDataChangedSetCvar(row, cvar)
     end
 end
 
-local function InitDashboard(dashP)
-    local cat = "TTT Bots 2"
-    local properties = vgui.Create("DProperties", dashP)
-    properties:Dock(FILL)
+local function getParsedNames()
+    local str = GetConVar("ttt_bot_names_custom"):GetString()
 
-    local lang = properties:CreateRow(cat, tr("mod.language"))
-    lang:Setup("Generic")
-    lang:SetValue(GetConVar("ttt_bot_language"):GetString())
-    onDataChangedSetCvar(lang, "ttt_bot_language")
+    -- This is basically a csv
+    local names = string.Split(str, ",")
+    return names
+end
 
-    local quotaN = properties:CreateRow(cat, tr("quota.num"))
-    quotaN:Setup("Int", { min = 0, max = game.MaxPlayers() or 0 })
-    quotaN:SetValue(GetConVar("ttt_bot_quota"):GetInt())
-    onDataChangedSetCvar(quotaN, "ttt_bot_quota")
+local function addNameToNames(str)
+    local content = GetConVar("ttt_bot_names_custom"):GetString()
+
+    local parsed = getParsedNames()
+    table.insert(parsed, str)
+
+    local filtered = {}
+    for i, v in pairs(parsed) do
+        parsed[i] = string.Trim(v)
+        if parsed[i] == "" then continue end
+        table.insert(filtered, parsed[i])
+    end
+
+    local content = table.concat(filtered, ",")
+
+    requestServerUpdateCvar("ttt_bot_names_custom", content)
+end
+
+local function InitNamesPanel(namesP)
+    local halfWide = 500 - 8
+
+    local rightPanel = vgui.Create("DPanel", namesP)
+    rightPanel:Dock(RIGHT)
+    rightPanel:SetWide(halfWide)
+
+    local rightNameList = vgui.Create("DListView", rightPanel)
+    rightNameList:Dock(FILL)
+    rightNameList:AddColumn(tr("name"))
+    rightNameList:SetMultiSelect(false)
+    rightNameList.ResetContent = function()
+        local lines = rightNameList:GetLines()
+        for i, line in ipairs(lines) do
+            rightNameList:RemoveLine(i)
+        end
+
+        local names = getParsedNames()
+        for i, str in pairs(names) do
+            rightNameList:AddLine(str)
+        end
+    end
+    rightNameList:ResetContent()
+
+    local leftPanel = vgui.Create("DPanel", namesP)
+    leftPanel:Dock(LEFT)
+    leftPanel:SetWide(halfWide)
+
+    local leftAddInput = vgui.Create("DTextEntry", leftPanel)
+    leftAddInput:Dock(TOP)
+    leftAddInput:DockMargin(0, 0, 0, 5)
+    leftAddInput:SetPlaceholderText(tr("add.name"))
+
+    local leftAddBtn = vgui.Create("DButton", leftPanel)
+    leftAddBtn:SetText(tr("add"))
+    leftAddBtn:Dock(TOP)
+    leftAddBtn:DockMargin(0, 0, 0, 5)
+    leftAddBtn.DoClick = function()
+        local str = leftAddInput:GetValue()
+        if str == "" then return end
+
+        addNameToNames(str)
+        leftAddInput:SetText("")
+
+        timer.Simple(0.5, rightNameList.ResetContent)
+    end
+
+    local leftRemoveBtn = vgui.Create("DButton", leftPanel)
+    leftRemoveBtn:SetText(tr("remove.selected"))
+    leftRemoveBtn:Dock(TOP)
+    leftRemoveBtn:DockMargin(0, 0, 0, 5)
+    leftRemoveBtn.DoClick = function()
+        local selected = rightNameList:GetSelectedLine()
+        if not selected then return end
+
+        local line = rightNameList:GetLine(selected)
+        local str = line:GetValue(1)
+
+        local names = getParsedNames()
+        local newNames = {}
+
+        for i, v in pairs(names) do
+            if v == str then continue end
+            table.insert(newNames, v)
+        end
+
+        local content = table.concat(newNames, ",")
+        requestServerUpdateCvar("ttt_bot_names_custom", content)
+
+        timer.Simple(0.5, rightNameList.ResetContent)
+    end
 end
 
 local function CreateBotMenu(ply)
@@ -58,12 +141,6 @@ local function CreateBotMenu(ply)
     local sheet = vgui.Create("DPropertySheet", window)
     sheet:Dock(FILL)
 
-    local dashP = vgui.Create("DPanel", sheet)
-    dashP:Dock(FILL)
-    -- dashP:DockPadding(padding, padding, padding, padding)
-    sheet:AddSheet(tr("dashboard"), dashP, "icon16/application.png")
-    InitDashboard(dashP)
-
     local botsP = vgui.Create("DPanel", sheet)
     botsP:Dock(FILL)
     -- botsP:DockPadding(padding, padding, padding, padding)
@@ -78,6 +155,7 @@ local function CreateBotMenu(ply)
     namesP:Dock(FILL)
     -- namesP:DockPadding(padding, padding, padding, padding)
     sheet:AddSheet(tr("bot.names"), namesP, "icon16/text_underline.png")
+    InitNamesPanel(namesP)
 
     local traitsP = vgui.Create("DPanel", sheet)
     traitsP:Dock(FILL)
