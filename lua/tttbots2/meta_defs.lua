7becae5bc96b113db1eb8836a29b04d80e6e9584
thebigsleepjoe <warmharddeath1@gmail.com>
2024-04-08 00:24:19 -0700

    Start working towards better linting

---
+++ lua/tttbots2/meta_defs.lua
@@ -0,0 +1,33 @@
+--- @meta
+--- This file exists to provide custom typing/linting for the addon. It
+--- has no inherent functionality and is never executed by the game.
+--- tbsj
+
+---@class Components : table
+---@field locomotor CLocomotor
+---@field memory CMemory
+---@field morality CMorality
+---@field chatter CChatter
+---@field inventory CInventory
+---@field obstacletracker CObstacleTracker
+---@field personality CPersonality
+
+---@class Vector
+---@field Distance fun(self: Vector, vec: Vector): number The distance from us to them.
+---@field LengthSqr fun(self: Vector): number The length of the vector prior to taking a sqrt.
+
+---@class Entity
+---@field GetPos fun(): Vector
+---@field OBBCenter fun(): Vector Object Bounding Box center of the Entity's model.
+---@field IsPlayer fun(): boolean Is the Entity a player?
+
+---@class Player : Entity
+---@field GetRoleStringRaw fun(): string
+---@field GetVelocity fun(): Vector
+---@field Nick fun(): string
+
+---@class Bot : Player
+---@field lastBarrelCheck number? The last time we checked for any nearby explosive barrels.
+---@field lastBarrel Entity? The last explosive barrel we detected in our search.
+---@field attackFocus number The focus of the bot's attack. Dictates accuracy, decreases with time.
+---@field components Components The components of the bot.
