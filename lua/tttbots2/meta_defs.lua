fd2c06922874770dff306bd46e19a60c7961f5ac
thebigsleepjoe <warmharddeath1@gmail.com>
2024-05-10 15:37:09 -0700

    Added definitions and made minor changes to BotLib for typing

--- lua/tttbots2/meta_defs.lua
+++ lua/tttbots2/meta_defs.lua
@@ -1,7 +1,15 @@
 --- @meta
 --- This file exists to provide custom typing/linting for the addon. It
---- has no inherent functionality and is never executed by the game.
+--- has no inherent functionality and is NEVER(!) executed by the game.
+--- This mod uses LuaLS annotations.
 --- tbsj
+
+ROLE_TRAITOR = 1
+game = {}
+player = {}
+file = {}
+TTT2 = {}
+MASK_SHOT = 0 -- some number, not literally 0.
 
 ---@class Components : table
 ---@field locomotor CLocomotor
@@ -15,16 +23,28 @@
 ---@class Vector
 ---@field Distance fun(self: Vector, vec: Vector): number The distance from us to them.
 ---@field LengthSqr fun(self: Vector): number The length of the vector prior to taking a sqrt.
+---@field Dot fun(self: Vector, vec: Vector): number The dot product of the two vectors.
 
 ---@class Entity
----@field GetPos fun(): Vector
----@field OBBCenter fun(): Vector Object Bounding Box center of the Entity's model.
----@field IsPlayer fun(): boolean Is the Entity a player?
+---@field GetPos fun(self: Entity): Vector
+---@field OBBCenter fun(self: Entity): Vector Object Bounding Box center of the Entity's model.
+---@field IsPlayer fun(self: Entity): boolean Is the Entity a player?
+---@field Visible fun(self: Entity, ent: Entity): boolean Can we see the Entity?
+---@field VisibleVec fun(self: Entity, vec: Vector): boolean Can we see the Vector?
+---@field EyePos fun(self: Entity): Vector The position of the Entity's eyes.
+---@field GetClass fun(self: Entity): string The class of the Entity.
+---@field GetAimVector fun(self: Entity): Vector The aim vector of the Entity.
+
+---@class Weapon : Entity
+---@field CanBuy table The roles that can buy this weapon, indexed by ROLE_ globals
+---@field AutoSpawnable boolean Whether this weapon can be spawned by the game.
 
 ---@class Player : Entity
----@field GetRoleStringRaw fun(): string
----@field GetVelocity fun(): Vector
----@field Nick fun(): string
+---@field GetRoleStringRaw fun(self: Entity): string
+---@field GetVelocity fun(self: Entity): Vector
+---@field Nick fun(self: Entity): string
+---@field UserID fun(self: Entity): string
+---@field GetActiveWeapon fun(self: Entity): Weapon the held wep of the player
 
 ---@class Bot : Player
 ---@field lastBarrelCheck number? The last time we checked for any nearby explosive barrels.
@@ -34,6 +54,10 @@
 ---@field redHandedTime number The time the bot was last seen committing a crime.
 
 ---@class CNavArea
+---@field GetVisibleAreas fun(self: CNavArea): table<CNavArea> Get all areas visible from this area.
+---@field GetAdjacentAreas fun(self: CNavArea): table<CNavArea> Get all areas adjacent to this area.
+---@field GetCenter fun(self: CNavArea): Vector Get the center of the area.
+
 ---@class CNavLadder
 
 ------------------------------------
@@ -61,3 +85,12 @@ function table.IsEmpty(tbl) return false end
 ---@param tbl table
 ---@return number
 function table.Count(tbl) return 0 end
+
+---@param str string
+---@param ending string
+---@return boolean
+function string.EndsWith(str, ending) return false end
+
+---Get the current epoch time.
+---@return number
+function SysTime() return 0 end
