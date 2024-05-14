21b5e036111a9f18aca97e93e53aa45acb505038
thebigsleepjoe <warmharddeath1@gmail.com>
2024-05-14 01:32:17 -0700

    Chore: refactor sound handling in sv_memory.lua for better readability and maintainability

--- lua/tttbots2/components/sv_memory.lua
+++ lua/tttbots2/components/sv_memory.lua
@@ -424,13 +424,14 @@ end
 ---@param soundData table The original GLua sound table.
 ---@return boolean IsUseful Whether or not the sound was useful, basically false if did not hear.
 function Memory:HandleSound(info, soundData)
-    local bot = self.bot
-    local soundpos = info.Pos
-    local stdrange = info.Distance
+    local bot = self.bot ---@type Bot
+    local soundPos = info.Pos
+    assert(soundPos, "Sound position is nil")
+    local standardRange = info.Distance
     local botHearingMult = self:GetHearingMultiplier()
 
-    local distTo = bot:GetPos():Distance(soundpos)
-    local canHear = distTo <= stdrange * botHearingMult
+    local distTo = bot:GetPos():Distance(soundPos)
+    local canHear = distTo <= standardRange * botHearingMult
 
     if not canHear then
         return false
@@ -439,7 +440,7 @@ function Memory:HandleSound(info, soundData)
     local tbl = {
         time = CurTime(),
         sound = info.SoundName,
-        pos = soundpos,
+        pos = soundPos,
         info = info,
         ent = info.EntInfo.Entity or info.EntInfo.Owner,
         sourceIsPly = info.EntInfo.EntityIsPlayer or info.EntInfo.OwnerIsPlayer,
@@ -461,10 +462,8 @@ function Memory:HandleSound(info, soundData)
     }
     local hashedName = pressureHash[info.SoundName]
     if hashedName then
-        local personality = lib.GetComp(bot, "personality")
-        if personality then
-            personality:OnPressureEvent(hashedName, tbl)
-        end
+        local personality = bot:BotPersonality()
+        personality:OnPressureEvent(hashedName)
     end
 
     return true
@@ -475,7 +474,7 @@ function Memory:CullSoundMemory()
     local recentSounds = self.recentSounds
     if not recentSounds then return end
     local curTime = CurTime()
-    for i, sound in pairs(recentSounds) do
+    for i, sound in ipairs(recentSounds) do
         local timeSince = curTime - sound.time
         if timeSince > 5 then
             table.remove(recentSounds, i)
