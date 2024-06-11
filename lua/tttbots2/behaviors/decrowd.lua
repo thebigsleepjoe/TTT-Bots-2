36a542acaf11e859e4ce339c9d7efc4c46f28cab
thebigsleepjoe <warmharddeath1@gmail.com>
2024-06-11 02:10:14 -0700

    Change decrowding behavior to consider players close by instead of just in sight

--- lua/tttbots2/behaviors/decrowd.lua
+++ lua/tttbots2/behaviors/decrowd.lua
@@ -14,9 +14,10 @@ Decrowd.Name = "Decrowd"
 Decrowd.Description = "Try to prevent overcrowding."
 Decrowd.Interruptible = true
 Decrowd.MaxNearbyPlayers = 3
+Decrowd.NearbyThreshold = 800 --- How close a player has to be to be considered "nearby"
 
 ---@class Bot
----@field targetRetreatArea CNavArea?
+---@field targetRetreatPos Vector?
 
 local STATUS = TTTBots.STATUS
 
@@ -30,20 +31,65 @@ function Decrowd.Validate(bot)
     local memory = bot:BotMemory()
     local visiblePlys = memory:GetRecentlySeenPlayers(5)
 
-    return (#visiblePlys > Decrowd.MaxNearbyPlayers)
+    local count = Decrowd.CountNearby(bot, visiblePlys)
+
+    return (count > Decrowd.MaxNearbyPlayers)
+end
+
+function Decrowd.CountNearby(bot, plyTable)
+    local count = 0
+    for i, ply in pairs(plyTable) do
+        local plyPos = ply:GetPos()
+        local dist = bot:GetPos():Distance(plyPos)
+        if dist < Decrowd.NearbyThreshold then
+            count = count + 1
+        end
+    end
+    return count
+
+end
+
+---Returns the number of living witnesses currently that can see pos (and are close-ish to it).
+---@param bot Bot
+---@param pos Vector
+---@return number
+function Decrowd.GetWitnessesAround(bot, pos)
+
+    local witnesses = TTTBots.Lib.GetAllWitnessesBasic(
+        pos,
+        TTTBots.Match.AlivePlayers,
+        bot
+    )
+
+    local count = Decrowd.CountNearby(bot, witnesses)
+
+    return count
+end
+
+---Tries to locate a hiding spot without a lot of witnesses.
+---Is not guaranteed to come up with a result due to performance reasons.
+---@param bot Bot
+---@return Vector?
+function Decrowd.FindRetreatSpot(bot)
+    local spots = TTTBots.Spots.GetSpotsInCategory("hiding")
+
+    for i = 1, 5 do
+        local randomSpot = table.Random(spots)
+        local nWitnesses = Decrowd.GetWitnessesAround(bot, randomSpot)
+
+        if nWitnesses <= 1 then
+            return randomSpot
+        end
+    end
 end
 
 function Decrowd.FindRetreatArea(bot)
     local botRegion = TTTBots.Lib.GetNearestRegion(bot:GetPos())
     for i = 1, 5 do
         local randomArea = TTTBots.Lib.GetRandomNavInRegion(botRegion)
-        local nWitnesses = TTTBots.Lib.GetAllWitnessesBasic(
-            randomArea:GetCenter(),
-            TTTBots.Match.AlivePlayers,
-            bot
-        )
+        local nWitnesses = Decrowd.GetWitnessesAround(bot, randomArea:GetPos())
 
-        if #nWitnesses <= 1 then
+        if nWitnesses <= 1 then
             return randomArea
         end
     end
@@ -54,14 +100,17 @@ end
 
 ---@param bot Bot
 function Decrowd.OnStart(bot)
-    bot.targetRetreatArea = Decrowd.FindRetreatArea(bot)
+    bot.targetRetreatPos = (
+        Decrowd.FindRetreatSpot(bot)
+        or Decrowd.FindRetreatArea(bot):GetPos()
+    )
     return STATUS.RUNNING
 end
 
 ---@param bot Bot
 function Decrowd.OnRunning(bot)
-    local targetPos = bot.targetRetreatArea:GetCenter()
-    bot:BotLocomotor():SetGoal(targetPos)
+    if not bot.targetRetreatPos then return Decrowd.OnStart(bot) end
+    bot:BotLocomotor():SetGoal(bot.targetRetreatPos)
 
     return STATUS.RUNNING
 end
